aa7fb3d5a3d15432c5974bf035ed27e73bd42f35
Guimauve48 <mathguimond@gmail.com>
2021-05-06 10:30:25 -0400

    add paper test1

--- mathieu/main.js
+++ mathieu/main.js
@@ -8,20 +8,42 @@ var npc3 = document.getElementById("npc3");
 function paper1Appear() {
   npc1.style.display = "none";
   paper1.style.display = "block";
-  paper1.style.left = "49%";
-  paper1.style.top = "49%";
+  paper1.style.left = "48%";
+  paper1.style.top = "47%";
+
+  var paper1center = setInterval(paper1ToMidlle, 10);
 }
 
 function paper2Appear() {
   npc2.style.display = "none";
   paper2.style.display = "block";
-  paper2.style.left = "49%";
-  paper2.style.top = "49%";
+  paper2.style.left = "48%";
+  paper2.style.top = "47%";
 }
 
 function paper3Appear() {
   npc3.style.display = "none";
   paper3.style.display = "block";
-  paper3.style.left = "49%";
-  paper3.style.top = "49%";
+  paper3.style.left = "48%";
+  paper3.style.top = "47%";
+}
+
+function paper1ToMiddle() {
+  var left1 = 35;
+  var top1 = 44;
+  var width1 = 3;
+  var counter1 = 0;
+
+  left1 = left1 + 0.13;
+  top1 = top1 + 0.03;
+  width1 = width1 + 0.1;
+  counter1 = counter1 + 1;
+
+  paper1.style.left = left1 + "%";
+  paper1.style.top = top1 + "%";
+  paper1.style.width = width1 + "%";
+
+  if (counter1 == 100) {
+    clearInterval(paper1center);
+  }
 }
